500c8aea90afd651288e5a64bc3b72e8746c7c6c
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-13 19:39:34 +0100

    Add Button component

---
+++ anecdotes/src/components/Button.tsx
@@ -0,0 +1,10 @@
+import { MouseEvent } from "react";
+
+type Props = {
+  text?: string;
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+};
+
+export const Button = ({ text, onClick }: Props) => {
+  return <button onClick={onClick}>{text}</button>;
+};
